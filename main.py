41bc4b64757184da4815fed1ffacfd023f82504c
PavelDenisov777 <153274777+PavelDenisov777@users.noreply.github.com>
2023-12-25 17:59:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ TOKEN = 'vk1.a.I7elr2wQctj_2n1XxGL2_LQf1UrIryjGg-gwxBa6zNReQmDS_C9_0Bpprluez3jDy
 vk_session = vk_api.VkApi(token=TOKEN)
 longpool = VkLongPoll(vk_session)
 vk = vk_session.get_api()
-vars = ['камень', 'ножн', 'бумага']
+vars = ['камень', 'ножницы', 'бумага']
 for event in longpool.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.text and event.to_me and event.text.lower() in vars:
         if event.from_user:
